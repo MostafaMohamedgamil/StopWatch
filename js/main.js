69433361353fce8c6f752d5cce350e3cdf5cf117
Mostafa Mohamed <mostafatop126@gmail.com>
2024-08-04 05:54:56 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,104 @@
+/*
+setinterval()
+*/
+
+const minLable = document.getElementById("min")
+const secLable = document.getElementById("sec")
+const millisecLable = document.getElementById("millisec")
+// Button
+const startButton = document.getElementById("startBtn")
+const stopButton = document.getElementById("stopBtn")
+const pauseButton = document.getElementById("pauseBtn")
+const resetButton = document.getElementById("resetBtn")
+
+const lapList = document.getElementById("laplist")
+
+// Stop Watch var
+
+let minutes = 0
+let seconds = 0;
+let milliseconds = 0
+let interval;
+
+startButton.addEventListener('click', startTimer)
+stopButton.addEventListener('click', stopTimer)
+pauseButton.addEventListener('click', pauseTimer)
+resetButton.addEventListener('click', resetTimer)
+
+function startTimer() {
+    interval = setInterval(updateTimer, 10)
+    startButton.disabled = true
+
+}
+function stopTimer() {
+
+    clearInterval(interval)
+    addToLapList()
+    resetTimerData()
+    startButton.disabled = false
+
+
+
+}
+
+function pauseTimer() {
+    clearInterval(interval)
+    // pauseButton.disabled = true
+    startButton.disabled = false;
+
+}
+function resetTimer() {
+    clearInterval(interval)
+    resetTimerData();
+    startButton.disabled = false;
+
+}
+
+function updateTimer() {  // 1sec =1000 miliseconds
+    milliseconds++;
+    if (milliseconds === 100) {
+        milliseconds = 0;
+        seconds++;
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++
+        }
+    }
+    displayTimer();
+}
+
+function displayTimer() {
+    millisecLable.textContent = padTime(milliseconds);
+    secLable.textContent = padTime(seconds);
+    minLable.textContent = padTime(minutes);
+}
+
+function padTime(time) {
+    return time.toString().padStart(2, "0")
+}
+
+
+function resetTimerData() {
+    minutes = 0
+    seconds = 0;
+    milliseconds = 0
+    displayTimer();
+}
+
+
+
+function addToLapList() {
+    const lapTime = `${padTime(minutes)}:${padTime(seconds)}:${padTime(milliseconds)}`;
+
+    const listItem = document.createElement('li')
+    listItem.innerHTML = `<span>Lap${lapList.childElementCount + 1}:</span>${lapTime}`;
+lapList.appendChild(listItem);
+
+}
+
+
+
+
+
+
+
